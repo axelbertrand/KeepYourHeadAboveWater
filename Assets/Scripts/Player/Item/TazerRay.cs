584874bcf62925c7feeabbd0991e88edcf91c682
clementcariou <clem494949@gmail.com>
2019-10-12 17:13:17 -0400

    tazer ray paralize the player

---
+++ Assets/Scripts/Player/Item/TazerRay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TazerRay : MonoBehaviour { 
+    void OnTriggerEnter(Collider2D collider){
+        PlayerController2 player = collider.GetComponent<PlayerController2>();
+        if (player)
+            StartCoroutine(paralize(player));
+    }
+
+    IEnumerator paralize(PlayerController2 player) {
+        player.runSpeed *= 0.1f;
+        yield return new WaitForSeconds(1f);
+        player.runSpeed /= 0.1f;
+    }
+}
