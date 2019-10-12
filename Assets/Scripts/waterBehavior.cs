208876b1106d0e6e835aea6a4c6095f5ef0d9315
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-12 14:44:54 -0400

    fix water behavior

--- Assets/Scripts/waterBehavior.cs
+++ Assets/Scripts/waterBehavior.cs
@@ -2,38 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class waterBehavior : MonoBehaviour
+public class WaterBehavior : MonoBehaviour
 {
-    
-    private int speed_ = 3;
+    public float speed = 0.5f;
 
-    [SerializeField]
-    public int deltaWater_ = 0;
-    private int currentPos_;
     // Start is called before the first frame update
     void Start()
     {
-        currentPos_ = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        ChangeLevel();
-    }
-
-    void ChangeLevel()
-    {
-        if(deltaWater_ != currentPos_)
-        {
-            int direction = (deltaWater_ - currentPos_ > 0) ? 1 : -1; 
-            transform.Translate(new Vector3(0,direction * speed_ * Time.deltaTime,0));
-            currentPos_+= direction;
-        }
-    }
-
-    public void setDelta(int delta)
-    {
-        deltaWater_ += delta;
+        transform.Translate(new Vector3(0f, Time.deltaTime * speed));
     }
 }
