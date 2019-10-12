e7e47e0f186cee64c47ab8bc9c028ca268316af9
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-12 18:11:43 -0400

    start end game

---
+++ Assets/Scripts/EndGameTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EndGameTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collided = collision.gameObject;
+        if (collided.tag == "Player")
+        {
+            print("Un joueur a gagné");
+            GameObject helicoptere = GameObject.Find("Helicoptere");
+            Sequence helicoptereSequence = DOTween.Sequence();
+            helicoptereSequence.Append(helicoptere.transform.DOMove(collided.transform.position, 1))
+                .AppendInterval(1)
+                .Append(helicoptere.transform.DOMove(collided.transform.position + new Vector3(0, 10), 1));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
