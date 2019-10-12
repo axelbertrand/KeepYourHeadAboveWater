920cef8e332a35df5663df87118dcac820623e02
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-12 15:53:26 -0400

    balance water speed

--- Assets/Scripts/waterBehavior.cs
+++ Assets/Scripts/waterBehavior.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterBehavior : MonoBehaviour
 {
-    public float speed = 0.5f;
+    public float speed = 1.25f;
 
     // Start is called before the first frame update
     void Start()
