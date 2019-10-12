02c21c5281aa58ad8db0d78e1cd023f5e2b7e324
Bertrand Gilbert-Collet <bertrand.gc@gmail.com>
2019-10-12 11:43:11 -0400

    Added my scene

---
+++ Assets/Follow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
