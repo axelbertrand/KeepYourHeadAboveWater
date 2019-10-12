fbf68bc7c1d3176b71fafb1575904794a3af0fd5
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-12 16:20:10 -0400

    hotfix class name

--- Assets/Scripts/waterBehavior.cs
+++ Assets/Scripts/waterBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterBehavior : MonoBehaviour
+public class waterBehavior : MonoBehaviour
 {
     public float speed = 1.25f;
 
