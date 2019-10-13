d36425ef01c22eed86fe5cd32d2a2088e378f7e1
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-13 10:51:12 -0400

    add helicopter sprite and player animation

--- Assets/Scripts/EndGameTrigger.cs
+++ Assets/Scripts/EndGameTrigger.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
 using UnityEngine.UI;
-using System.Threading.Tasks;
+using Cinemachine;
 
 public class EndGameTrigger : MonoBehaviour
 {
@@ -17,19 +18,61 @@ public class EndGameTrigger : MonoBehaviour
         if (collided.tag == "Player")
         {
             Debug.Log("Un joueur a gagné !");
-            collided.GetComponentInParent<PlayerController2>().enabled = false;
 
+            // Focus the camera on the winner
+            CinemachineTargetGroup targetGroupComponent = GameObject.Find("TargetGroup").GetComponent<CinemachineTargetGroup>();
+            List<CinemachineTargetGroup.Target> targetList = new List<CinemachineTargetGroup.Target>(targetGroupComponent.m_Targets);
+            CinemachineTargetGroup.Target winnerTarget = targetList.Find(target => target.target == collided.transform.parent);
+            winnerTarget.radius = 10;
+            targetList.Clear();
+            targetList.Add(winnerTarget);
+            targetGroupComponent.m_Targets = targetList.ToArray();
+
+            // Disable player control
+            collided.GetComponentInParent<PlayerController2>().enabled = false;
+            collided.GetComponentInParent<Prime31.CharacterController2D>().enabled = false;
             GameObject helicopter = GameObject.Find("Helicopter");
 
-            Vector3 helicopterLandingPosition = levelSurface.position + new Vector3(-levelSurface.localScale.x / 4, 3 * helicopter.transform.localScale.y + levelSurface.localScale.y / 2);
+            // Disable collisions with the object being attached
+            BoxCollider2D collider = collided.GetComponentInParent<BoxCollider2D>();
+            if (collider != null)
+            {
+                collider.enabled = false;
+            }
+
+            // Don't allow physics to affect the object
+            Rigidbody2D rigidBody = collided.GetComponentInParent<Rigidbody2D>();
+            if (rigidBody != null)
+            {
+                rigidBody.isKinematic = true;
+            }
+
+            Vector3 helicopterLandingPosition = levelSurface.position + new Vector3(-levelSurface.localScale.x / 4, 2 * helicopter.transform.localScale.y + levelSurface.localScale.y / 2);
+            Vector3 winnerPosition = new Vector3(helicopterLandingPosition.x, levelSurface.position.y + collided.transform.parent.localScale.y / 2 + levelSurface.localScale.y / 2);
             Vector3[] waypoints = new Vector3[] { new Vector3(levelSurface.position.x, helicopter.transform.position.y), helicopterLandingPosition };
+            
             DOTween.Sequence()
+                // Move winner to the landing spot
+                .Append(collided.transform.parent.DOMove(winnerPosition, 1))
+                // Move helicopter to the landing spot
                 .Append(helicopter.transform.DOPath(waypoints, 3, PathType.CatmullRom).SetEase(Ease.OutQuad))
                 .AppendInterval(1)
-                .Append(helicopter.transform.DOMove(helicopterLandingPosition + new Vector3(0, 10), 2).SetEase(Ease.InQuad))
-                .OnComplete(() => GameObject.Find("EndGameText").GetComponent<Text>().enabled = true );
-
-            collided.transform.DOMove(helicopterLandingPosition, 1);
+                // Attach winner to helicopter
+                .AppendCallback(() =>
+                {
+                    collided.transform.parent.SetParent(helicopter.transform);
+                    collided.transform.parent.localPosition = Vector3.zero;
+                    CinemachineVirtualCamera camera2D = GameObject.Find("2DCamera").GetComponent<CinemachineVirtualCamera>();
+                    camera2D.Follow = null;
+                    camera2D.LookAt = collided.transform.parent;
+                })
+                // Helicopter landing off
+                .Append(helicopter.transform.DOMove(helicopterLandingPosition + new Vector3(0, 15), 2).SetEase(Ease.InQuad))
+                // When finished show end game menu
+                .OnComplete(() =>
+                {
+                    GameObject.Find("EndGameText").GetComponent<Text>().enabled = true;
+                });
         }
     }
 }
