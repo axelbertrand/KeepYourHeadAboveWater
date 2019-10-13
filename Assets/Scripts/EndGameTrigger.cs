e66b04fcc1c509154ad7836c61b871714b921408
axelbertrand <axel.bertrand2@etu.univ-lyon1.fr>
2019-10-12 23:52:15 -0400

    disable player control when entering trigger

--- Assets/Scripts/EndGameTrigger.cs
+++ Assets/Scripts/EndGameTrigger.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
 public class EndGameTrigger : MonoBehaviour
 {
     [SerializeField]
     private Transform levelSurface;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,19 +17,19 @@ public class EndGameTrigger : MonoBehaviour
         if (collided.tag == "Player")
         {
             Debug.Log("Un joueur a gagné !");
+            collided.GetComponentInParent<PlayerController2>().enabled = false;
+
             GameObject helicopter = GameObject.Find("Helicopter");
 
             Vector3 helicopterLandingPosition = levelSurface.position + new Vector3(-levelSurface.localScale.x / 4, 3 * helicopter.transform.localScale.y + levelSurface.localScale.y / 2);
             Vector3[] waypoints = new Vector3[] { new Vector3(levelSurface.position.x, helicopter.transform.position.y), helicopterLandingPosition };
             DOTween.Sequence()
-                .Append(helicopter.transform.DOPath(waypoints, 2, PathType.CatmullRom).SetEase(Ease.OutQuad))
+                .Append(helicopter.transform.DOPath(waypoints, 3, PathType.CatmullRom).SetEase(Ease.OutQuad))
                 .AppendInterval(1)
-                .Append(helicopter.transform.DOMove(helicopterLandingPosition + new Vector3(0, 10), 2).SetEase(Ease.InQuad));
-        }
-    }
+                .Append(helicopter.transform.DOMove(helicopterLandingPosition + new Vector3(0, 10), 2).SetEase(Ease.InQuad))
+                .OnComplete(() => GameObject.Find("EndGameText").GetComponent<Text>().enabled = true );
 
-    // Update is called once per frame
-    void Update()
-    {
+            collided.transform.DOMove(helicopterLandingPosition, 1);
+        }
     }
 }
